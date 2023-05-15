20beefe2a5130e7d8a0db8112d17237de2c54631
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-05-15 23:21:44 +0530

    created folders and reseting css

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Navbar.module.css'
+function Navbar() {
+    return (
+        <div className={style.navcontainer}>
+            
+            <h2>EDYODA</h2>
+
+        </div>
+    )
+}
+
+export default Navbar
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
 
 function Home() {
     return (
         <div>
+            <Navbar/>
             Home
         </div>
     )
