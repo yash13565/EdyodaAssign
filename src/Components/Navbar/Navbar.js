9dae77343630ead3760021ff9b70181197c74071
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-05-15 23:29:36 +0530

    Added Dependency

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,9 +3,14 @@ import style from './Navbar.module.css'
 function Navbar() {
     return (
         <div className={style.navcontainer}>
-            
+            <div className={style.rightpart}>
             <h2>EDYODA</h2>
+            <h5>Courses</h5>
+            <h5>Program</h5>
+            </div>
+            <div className={style.endpart}> 
 
+            </div>
         </div>
     )
 }
